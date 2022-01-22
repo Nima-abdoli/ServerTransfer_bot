68c772a0942cd5e2c163f292cea3e2c2ec355ac7
Nima abdoli <23312148+Nima-abdoli@users.noreply.github.com>
2022-01-22 20:57:16 +0330

    check type of Os between Linux and Windows

---
+++ FileExplorer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTransfer_bot
+{
+    public class FileExplorer
+    {
+        bool isLinux;
+
+        public void OsCheck()
+        {
+            if (OperatingSystem.IsLinux())
+            {
+                isLinux = true;
+            }
+            if (OperatingSystem.IsWindows())
+            {
+                isLinux = false;
+            }
+        }
+
+
+    }
+}

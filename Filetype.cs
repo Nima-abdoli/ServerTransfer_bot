55d01e5460a100c53b878653d6fccc084b67eecb
Nima abdoli <nimaabdoli@outlook.com>
2022-01-28 23:53:15 +0330

    determine File or Folder
    to make sure the item in list is folder or file make new class

---
+++ Filetype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTransfer_bot
+{
+    public class FileType
+    {
+        public string Filename { get; set; }
+        public bool IsFolder;
+    }
+}
